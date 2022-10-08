d680e6dac91fa5425daeeca6a012dc0d8b80821b
Ivan Hawkes <ivan.hawkes@gmail.com>
2022-10-08 14:20:45 +1000

    Removing MPG and preparing to add TomlPlusPlus.

---
+++ centre-module/include/GamepadEnums.h
@@ -0,0 +1,58 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2021 Jason Skuby (mytechtoybox.com)
+ */
+
+#pragma once
+
+// The available input modes
+// TODO: Think about making enums "class enum" instead. This will keep them out of the global namespace
+// and provide strong type assurance instead of being treated as ints. Also, modern c++ would tend towards
+// using Pascal-case naming for the individual declarations.
+enum InputMode
+{
+	INPUT_MODE_XINPUT,
+	INPUT_MODE_SWITCH,
+	INPUT_MODE_HID,
+	INPUT_MODE_CONFIG = 255,
+};
+
+// The available stick emulation modes
+enum DpadMode
+{
+	DPAD_MODE_DIGITAL,
+	DPAD_MODE_LEFT_ANALOG,
+	DPAD_MODE_RIGHT_ANALOG,
+};
+
+// The available SOCD cleaning methods
+enum SOCDMode
+{
+	SOCD_MODE_UP_PRIORITY,           // U+D=U, L+R=N
+	SOCD_MODE_NEUTRAL,               // U+D=N, L+R=N
+	SOCD_MODE_SECOND_INPUT_PRIORITY, // U>D=D, L>R=R (Last Input Priority, aka Last Win)
+};
+
+// Enum for tracking last direction state of Second Input SOCD method
+enum DpadDirection
+{
+	DIRECTION_NONE,
+	DIRECTION_UP,
+	DIRECTION_DOWN,
+	DIRECTION_LEFT,
+	DIRECTION_RIGHT
+};
+
+// The available hotkey actions
+enum GamepadHotkey
+{
+	HOTKEY_NONE = 0x00,
+	HOTKEY_DPAD_DIGITAL = (1U << 0),
+	HOTKEY_DPAD_LEFT_ANALOG = (1U << 1),
+	HOTKEY_DPAD_RIGHT_ANALOG = (1U << 2),
+	HOTKEY_HOME_BUTTON = (1U << 3),
+	HOTKEY_CAPTURE_BUTTON = (1U << 4),
+	HOTKEY_SOCD_UP_PRIORITY = (1U << 5),
+	HOTKEY_SOCD_NEUTRAL = (1U << 6),
+	HOTKEY_SOCD_LAST_INPUT = (1U << 7),
+};
--- centre-module/src/DigitalInput.cpp
+++ centre-module/src/DigitalInput.cpp
@@ -1,49 +1,49 @@
 #include "DigitalInput.h"
 
-#include "GamepadState.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "tusb.h"
 
 
-class DigitalInput switchArray[]{
-    // Joystick.
-    {0, GAMEPAD_BUTTON_0, "Joy Down"},  // Down
-    {1, GAMEPAD_BUTTON_1, "Joy Up"},    // Up
-    {2, GAMEPAD_BUTTON_2, "Joy Left"},  // Left
-    {3, GAMEPAD_BUTTON_3, "Joy Right"}, // Right
+class DigitalInput switchArray[]
+{
+	// Joystick.
+	{0, GAMEPAD_BUTTON_0, "Joy Down"},      // Down
+	    {1, GAMEPAD_BUTTON_1, "Joy Up"},    // Up
+	    {2, GAMEPAD_BUTTON_2, "Joy Left"},  // Left
+	    {3, GAMEPAD_BUTTON_3, "Joy Right"}, // Right
 
-    // Lower row of top panel buttons (left to right).
-    {4, GAMEPAD_BUTTON_4, "B1"}, // 1k, B1, A, Circle
-    {5, GAMEPAD_BUTTON_5, "B2"}, // 2k, B2, B, Cross
-    {6, GAMEPAD_BUTTON_8, "R2"}, // 3k, R2, RT
-    {7, GAMEPAD_BUTTON_9, "L2"}, // 4k, L2, LT
+	    // Lower row of top panel buttons (left to right).
+	    {4, GAMEPAD_BUTTON_4, "B1"}, // 1k, B1, A, Circle
+	    {5, GAMEPAD_BUTTON_5, "B2"}, // 2k, B2, B, Cross
+	    {6, GAMEPAD_BUTTON_8, "R2"}, // 3k, R2, RT
+	    {7, GAMEPAD_BUTTON_9, "L2"}, // 4k, L2, LT
 
-    // Upper row of top panel buttons (left to right).
-    {8, GAMEPAD_BUTTON_6, "B3"},   // 1p, B3, X, Triangle
-    {9, GAMEPAD_BUTTON_7, "B4"},   // 2p, B4, Y, Square
-    {10, GAMEPAD_BUTTON_10, "R1"}, // 3p, R1, RB
-    {11, GAMEPAD_BUTTON_11, "L1"}, // 4p, L1, LB
+	    // Upper row of top panel buttons (left to right).
+	    {8, GAMEPAD_BUTTON_6, "B3"},   // 1p, B3, X, Triangle
+	    {9, GAMEPAD_BUTTON_7, "B4"},   // 2p, B4, Y, Square
+	    {10, GAMEPAD_BUTTON_10, "R1"}, // 3p, R1, RB
+	    {11, GAMEPAD_BUTTON_11, "L1"}, // 4p, L1, LB
 
-    // Left panel buttons.
-    {12, GAMEPAD_BUTTON_12, "-12-"}, // Used for LED I think.
-    {13, GAMEPAD_BUTTON_13, "-13-"}, // Used for LED I think.
+	    // Left panel buttons.
+	    {12, GAMEPAD_BUTTON_12, "-12-"}, // Used for LED I think.
+	    {13, GAMEPAD_BUTTON_13, "-13-"}, // Used for LED I think.
 
-    // Rear panel buttons.
-    {14, GAMEPAD_BUTTON_SELECT, "S1"}, // Select, S1, Back
-    {15, GAMEPAD_BUTTON_START, "S2"},  // Start, S2, Start
+	    // Rear panel buttons.
+	    {14, GAMEPAD_BUTTON_SELECT, "S1"}, // Select, S1, Back
+	    {15, GAMEPAD_BUTTON_START, "S2"},  // Start, S2, Start
 
-    // Right panel buttons.
-    // NOTE: Currently using 16+17 for UART - may change with Hydra board.
-    // {16, GAMEPAD_BUTTON_16, "L3"}, // LS, L3, LS
-    // {17, GAMEPAD_BUTTON_17, "R3"}, // RS, R3, RS
+	    // Right panel buttons.
+	    // NOTE: Currently using 16+17 for UART - may change with Hydra board.
+	    // {16, GAMEPAD_BUTTON_16, "L3"}, // LS, L3, LS
+	    // {17, GAMEPAD_BUTTON_17, "R3"}, // RS, R3, RS
 
-    // Front panel buttons (left to right).
-    {18, GAMEPAD_BUTTON_18, "A1"}, // Home, A1, XBOX
-    {19, GAMEPAD_BUTTON_19, "A2"}, // TP, A2, -
+	    // Front panel buttons (left to right).
+	    {18, GAMEPAD_BUTTON_18, "A1"}, // Home, A1, XBOX
+	    {19, GAMEPAD_BUTTON_19, "A2"}, // TP, A2, -
 
-    // Top panel. Extra
-    {20, GAMEPAD_BUTTON_20, "Insert Coin"}, // Insert coin
+	    // Top panel. Extra
+	    {20, GAMEPAD_BUTTON_20, "Insert Coin"}, // Insert coin
 };
 
 
